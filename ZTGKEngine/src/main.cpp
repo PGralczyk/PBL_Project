5d795b9da82648938b7a0edd2b5af0aa1970e4ed
Bamaluko <kuchciakkacper@gmail.com>
2023-03-17 20:15:41 +0100

    Adding glad and changing the background color for the magnificient effin' window
    What it says above

--- ZTGKEngine/src/main.cpp
+++ ZTGKEngine/src/main.cpp
@@ -1,4 +1,22 @@
+#include <iostream>
 #include <GLFW/glfw3.h>
+//#include <glad/glad.h>
+
+#define IMGUI_IMPL_OPENGL_LOADER_GLAD
+
+#if defined(IMGUI_IMPL_OPENGL_LOADER_GL3W)
+#include <GL/gl3w.h>    // Initialize with gl3wInit()
+#elif defined(IMGUI_IMPL_OPENGL_LOADER_GLEW)
+#include <GL/glew.h>    // Initialize with glewInit()
+#elif defined(IMGUI_IMPL_OPENGL_LOADER_GLAD)
+#include <glad/glad.h>  // Initialize with gladLoadGL()
+#else
+#include IMGUI_IMPL_OPENGL_LOADER_CUSTOM
+#endif
+
+//For now I keep those functions in here
+void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+void processInput(GLFWwindow* window);
 
 int main(void)
 {
@@ -7,6 +25,10 @@ int main(void)
     /* Initialize the library */
     if (!glfwInit())
         return -1;
+
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
     /* Create a windowed mode window and its OpenGL context */
     window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
@@ -18,12 +40,26 @@ int main(void)
 
     /* Make the window's context current */
     glfwMakeContextCurrent(window);
+    //Below I set the function to be used in case our window changed size
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+    //I try to use Glad as adresses loader and i realy hope it works
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+    {
+        std::cout << "Failed to initialize GLAD" << std::endl;
+        return -1;
+    }
 
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
     {
+        //Processing input here
+        processInput(window);
+
+        glClearColor(0.45f, 0.55f, 0.60f, 1.00f);
+
         /* Render here */
-        glClear(GL_COLOR_BUFFER_BIT);
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
         /* Swap front and back buffers */
         glfwSwapBuffers(window);
@@ -34,4 +70,21 @@ int main(void)
 
     glfwTerminate();
     return 0;
+}
+
+// process all input: query GLFW whether relevant keys are pressed/released this frame and react accordingly
+// ---------------------------------------------------------------------------------------------------------
+void processInput(GLFWwindow* window)
+{
+    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, true);
+}
+
+// glfw: whenever the window size changed (by OS or user resize) this callback function executes
+// ---------------------------------------------------------------------------------------------
+void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+    // make sure the viewport matches the new window dimensions; note that width and 
+    // height will be significantly larger than specified on retina displays.
+    glViewport(0, 0, width, height);
 }
