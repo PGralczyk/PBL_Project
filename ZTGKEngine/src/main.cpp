149c766640d0b62dfccd7bbe10679938f2b4d1fd
Bamaluko <kuchciakkacper@gmail.com>
2023-03-18 12:57:31 +0100

    Deleting assimp, because it wasn't instaled properly

--- ZTGKEngine/src/main.cpp
+++ ZTGKEngine/src/main.cpp
@@ -5,9 +5,6 @@
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtc/type_ptr.hpp>
 #include <stb_image/stb_image.h>
-#include <assimp/Importer.hpp>
-#include <assimp/scene.h>
-#include <assimp/postprocess.h>
 
 //For now I keep those functions in here
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
