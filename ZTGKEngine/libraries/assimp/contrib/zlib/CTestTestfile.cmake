9710478c1df078b4f2ca48d88307a35b27612e52
PGralczyk <92301627+PGralczyk@users.noreply.github.com>
2023-03-19 19:17:04 +0100

    Contrib fix
    Just in case it explodes xd

---
+++ ZTGKEngine/libraries/assimp/contrib/zlib/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/zygmu/assimp/assimp/contrib/zlib
+# Build directory: C:/Users/zygmu/assimp/assimp/contrib/zlib
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
